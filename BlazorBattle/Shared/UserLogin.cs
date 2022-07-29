9b6d9df93d6af7ab206f90d2d4dd37f75a6c66a6
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-07-29 15:53:26 -0400

    form up for registration

---
+++ BlazorBattle/Shared/UserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Shared
+{
+    public class UserLogin
+    {
+        [Required (ErrorMessage = "Please enter a UserName.")]
+
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please enter a Password.")]
+
+        public string Password { get; set; }
+    }
+}
