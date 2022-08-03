d2ee07287b31a85aa5300de7fea96f342633af83
Ayeager2 <annanyeager@gmail.com>
2022-08-03 14:00:34 -0400

    BATTLE!!!

---
+++ BlazorBattle/Shared/BattleResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Shared
+{
+    public class BattleResult
+    {
+        public IList<string> BattleLog { get; set; } = new List<string>();
+        public int  AttackDamageSum { get; set; }
+        public int OpponentDamageSum { get; set; }
+        public bool IsVictory { get; set; }
+        public int RoundsFought { get; set; }
+    }
+}
