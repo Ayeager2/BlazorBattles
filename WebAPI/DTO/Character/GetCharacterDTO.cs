601085fde955d4e4275e3e266f9bcea166e61657
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-08-25 08:59:38 -0400

    Update GetCharacterDTO.cs
    Weapons now show in api call for getallcharacters

--- WebAPI/DTO/Character/GetCharacterDTO.cs
+++ WebAPI/DTO/Character/GetCharacterDTO.cs
@@ -15,7 +15,7 @@ namespace WebAPI.DTO.Character
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
-        public GetWeaponDto Weapons { get; set; }
+        public GetWeaponDto Weapon { get; set; }
         public List<GetSkillDto> Skills { get; set; }
     }
 }
